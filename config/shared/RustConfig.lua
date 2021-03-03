7cf1a086d426c370df93092311b5e292ab5c4476
benank <terkolskie@gmail.com>
2021-03-02 18:11:39 -0800

    Add global rust config

---
+++ config/shared/RustConfig.lua
@@ -0,0 +1,22 @@
+RustConfig = 
+{
+    --[[
+        Global multiplier for resource gathering.
+        Applies to: trees, stones, metal
+    ]]
+    ResourceGatherMultiplier = 1,
+
+    --[[
+        Maximum size of groups
+        Set to 0 to enable groups of any size
+    ]]
+    MaxGroupSize = 4,
+
+    --[[
+        Global multiplier for amount of resources needed for
+        base upkeep per day.
+
+        Set to 0 to allow bases to never decay (unless tool cupboard is destroyed)
+    ]]
+    BaseUpkeepModifier = 1,
+}
