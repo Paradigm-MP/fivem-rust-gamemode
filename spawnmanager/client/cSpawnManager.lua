6a0c8867b70378468bb676cfaffbab0d971f43cc
benank <terkolskie@gmail.com>
2021-03-13 16:52:45 -0800

    Make player naked

--- spawnmanager/client/cSpawnManager.lua
+++ spawnmanager/client/cSpawnManager.lua
@@ -15,45 +15,72 @@ function cSpawnManager:LoadingScreenFinished()
         model = "mp_m_freemode_01",
         callback = function()
             LocalPlayer:GetPed():GiveWeapon(GetHashKey("weapon_stone_hatchet"), 1, true)
-            -- math.randomseed(LocalPlayer:GetPlayer():GetUniqueId())
+            math.randomseed(LocalPlayer:GetPlayer():GetUniqueId())
 
             -- Give ped random attributes
             local ped = LocalPlayer:GetPed()
-
-            -- SetPedDefaultComponentVariation Default clothes
-
-            for i = 0, 12 do
-                ped:SetHeadOverlay(
-                    i,
-                    math.random(0, GetPedHeadOverlayNum(i)),
-                    math.random()
-                )
-
-            end
 
             -- Skin color
             SetPedHeadBlendData(ped.ped_id, 0, 0, 0, 15, 0, 0, 0, 0, math.random(), false)
 
             -- SetPedFaceFeature
             -- SetPedHairColor
-            -- SetPedEyeColor
+            SetPedHairColor(ped.ped_id, math.random(63), math.random(63))
+            SetPedEyeColor(ped.ped_id, math.random(30))
+
+            for i = 0, 20 do
+                SetPedFaceFeature(ped.ped_id, i, math.random() * 2 - 1)
+            end
             -- 
 
             -- Hair
             local id1 = 2
-            local id2 = math.random(GetNumberOfPedDrawableVariations(ped.ped_id, 2))
-            local id3 = math.random(GetNumberOfPedTextureVariations(ped.ped_id, 2))
+            local id2 = math.random(GetNumberOfPedDrawableVariations(ped.ped_id, id1))
+            local id3 = math.random(GetNumberOfPedTextureVariations(ped.ped_id, id1, id2))
             SetPedComponentVariation(ped.ped_id, 
                 id1,
                 id2, 
                 id3,
                 math.random(0, 3)
             )
-                
+      
+            -- Top
+            SetPedComponentVariation(ped.ped_id, 
+                11,
+                15, 
+                0,
+                0
+            )
+
+            -- Undershirt
+            SetPedComponentVariation(ped.ped_id, 
+                8,
+                15, 
+                0,
+                0
+            )
+
+            -- Shirt
             SetPedComponentVariation(ped.ped_id, 
                 3,
                 15, 
-                15,
+                0,
+                0
+            )
+
+            -- Pants
+            SetPedComponentVariation(ped.ped_id, 
+                4,
+                21, 
+                0,
+                0
+            )
+
+            -- Feet
+            SetPedComponentVariation(ped.ped_id, 
+                6,
+                34, 
+                0,
                 0
             )
 
